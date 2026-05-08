91453871575162dcece5a311e82680fb4920d14e
tech2900 <walimoo@gmail.com>
2026-05-09 00:17:56 +0100

    seo: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,26 @@
 const nextConfig = {
   poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.solanarefund.xyz" }],
+        destination: "https://solanarefund.xyz/:path*",
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
-    return [{
-      source: "/(.*)",
-      headers: [
-        { key: "X-Content-Type-Options", value: "nosniff" },
-        { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-        { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=(), payment=()" },
-      ]
-    }]
-  }
-}
-export default nextConfig
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=(), payment=()" },
+        ],
+      },
+    ];
+  },
+};
+export default nextConfig;
